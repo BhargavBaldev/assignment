943b1ab0b6ac6e3a511a274cffd80e8684c04ef3
Bhargav Baldev <bhargav.baldev@aimdek.com>
2020-02-07 17:30:09 +0530

    react app changes

--- Web Technologies/ReactAssignment/my-app/src/App.js
+++ Web Technologies/ReactAssignment/my-app/src/App.js
@@ -3,26 +3,20 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import {css, withStyles} from "react-with-styles";
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 
 
-
-export class App extends React.Component {
-  
+export default class App extends React.Component {
   
   state = { list: [] };
-  
   onClick = e => {
     if(this.state.uname===''){
       alert("user name should not be empty");
     }else{
     this.state.list.push(this.state)
-    this.setState({ uname:null, password:null });
+    this.setState({uname:'',password:''});
     }
   };
-  
   changeuname = e => {
     this.setState({ uname: e.target.value });
   };
@@ -41,10 +35,11 @@ export class App extends React.Component {
               Show
             </Button>
           </form>
-          <div {...css(styles.root)}>
+          <div>
             <Grid container spacing={3}>
               <Grid item xs={12}>
-                <Paper {...css(styles.paper)}>{this.state.list.map((person) => (<p>User name: {person.uname}<br/> Password {person.password}</p>))}</Paper>
+                {this.state.list.map((person) => (
+                  <p>User name: {person.uname}<br/> Password {person.password}</p>))}
               </Grid>
             </Grid>
           </div>  
@@ -53,13 +48,3 @@ export class App extends React.Component {
     );
   }
 }
-export default withStyles(({flexGrow,padding,textAlign,color})=>({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: padding.spacing(2),
-    textAlign: 'center',
-    color: color.secondary,
-  },
-}))(App);
